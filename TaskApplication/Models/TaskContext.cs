635aad6045d37cce115398f3891d0e28707ea034
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-03-01 20:56:41 -0700

    Stuff again

---
+++ TaskApplication/Models/TaskContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskApplication.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }    
+    }
+}
